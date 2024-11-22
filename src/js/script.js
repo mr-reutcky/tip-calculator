6a21a131bc5f82425d3b4ecbf2dd30248d989db9
Samuel Reutcky <sreutcky@gmail.com>
2024-11-22 12:43:15 -0600

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function select(selector, scope = document) {
+  return scope.querySelector(selector);
+}
+
+function listen(event, selector, callback) {
+  return selector.addEventListener(event, callback);
+}
